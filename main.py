bd605bbc150326a0cd7e16c46c9e50bfef1cb94e
graaf-mc <aleijen@gmail.com>
2025-12-10 11:14:49 +0100

    completed books project

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from stats import get_number_of_words, get_number_of_chars
+from stats import get_book_text, get_number_of_words, get_number_of_chars, sorted_char_num_list
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = get_number_of_words(book_text)
     num_chars = get_number_of_chars(book_text)
+    report_list = sorted_char_num_list(num_chars)
     # print("=== Here would have been printed full_book_text ===")
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    print(f"Found the following chars: {num_chars}")
+    for char in report_list:
+        if char["char"].isalpha():
+            print(f"'{char['char']}': {char['num']}")
     print("============= END ===============")
-    print(num_chars.items())
-
-def get_book_text(file_path):
-    with open(file_path) as f:
-        return f.read()
     
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
 def get_number_of_words(full_text):
     num_words = full_text.split()
     return len(num_words)
@@ -8,6 +12,13 @@ def get_number_of_chars(full_text):
     for char in lower_text:
         counter[char] = counter.get(char, 0) + 1
     return counter
+
+def sorted_char_num_list(all_chars):
+    sorted_list = []
+    for chars in all_chars:
+        sorted_list.append({"char": chars, "num" :all_chars[chars]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def sort_on(items):
     return items["num"]
