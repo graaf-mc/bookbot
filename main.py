0adbcfea25310cb815b4462472f24ed63ac2cdc5
graaf-mc <aleijen@gmail.com>
2025-12-10 12:03:22 +0100

    Finalized books Report

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import get_book_text, get_number_of_words, get_number_of_chars, sorted_char_num_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_number_of_words(book_text)
     num_chars = get_number_of_chars(book_text)
@@ -14,7 +19,7 @@ def main():
     print("--------- Character Count -------")
     for char in report_list:
         if char["char"].isalpha():
-            print(f"'{char['char']}': {char['num']}")
+            print(f"{char['char']}: {char['num']}")
     print("============= END ===============")
     
 if __name__ == "__main__":
