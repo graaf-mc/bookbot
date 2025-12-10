b7b3430b20a7c3a76fb4f6fa05dbdb2adc256ba3
graaf-mc <aleijen@gmail.com>
2025-12-10 08:36:10 +0100

    update on books

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_number_of_words(full_text):
     num_words = full_text.split()
     return len(num_words)
+
+def get_number_of_chars(full_text):
+    counter = {}
+    lower_text = full_text.lower()
+    for char in lower_text:
+        counter[char] = counter.get(char, 0) + 1
+    return counter
+
+def sort_on(items):
+    return items["num"]
+
